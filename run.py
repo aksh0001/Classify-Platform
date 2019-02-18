2b84983218183fbf8900d08636df0c39e4177be1
Johnny Liaw <jxlia3@student.monash.edu>
2019-02-18 12:32:50 +1100

    database created
    Instantiated a Lawsuit and Lawfirm database using SQLAlchemy

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/Johnny1/Documents/GitHub/flask101/classify.db'
+db = SQLAlchemy(app)
+from models import Lawfirm, Lawsuit
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)

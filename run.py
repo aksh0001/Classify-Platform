bab6fc4639f5653c41df8adb5e3c06b8d51fe3b9
Johnny Liaw <jxlia3@student.monash.edu>
2019-02-23 15:27:51 +1100

    Third commit
    Additions to add the signup form

--- run.py
+++ run.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/Johnny1/Documents/GitHub/flask101/classify.db'
-db = SQLAlchemy(app)
-from models import Lawfirm, Lawsuit
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup_form.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
